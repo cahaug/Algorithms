11601416b2d2befd049e070326002079766375a7
cahaug <caleahaug@gmail.com>
2020-03-02 20:10:37 -0700

    getting started

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 
 import argparse
-
+# prices = [int, int, int]
 def find_max_profit(prices):
-  pass
+  # duplicate the array for manipulation
+  # look at each price, save the highest & lowest prices
+  # the index of the lowest price must be less than the index of the highest price
+  # find the difference between each value and the values after it, saving the greatest difference (most profit)
+  # loop through all values until the last two in the array are the only available
+  # -> make every combination of possible buy and see which has the greatest profit making potential
 
 
 if __name__ == '__main__':
