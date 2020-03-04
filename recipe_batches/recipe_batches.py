7157bee1c49a725931dd2a6c2041055909810f52
cahaug <caleahaug@gmail.com>
2020-03-03 18:53:20 -0700

    test passing for recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,31 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  cookings_count = []
+  print(recipe.keys())
+  for key in recipe.keys():
+    # print(key)
+    if key not in ingredients:
+      return 0
+    current_max_cookings = ingredients[key] // recipe[key]
+    # print(f'recipe key: {key}: {recipe[key]}, ingredients: {ingredients[key]}')
+    # print(current_max_cookings)
+    cookings_count.append(current_max_cookings)
+    # print(cookings_count)
+  # print(cookings_count)      
+  max_possible_cookings = min(cookings_count)
+  return max_possible_cookings
+      
+# print('division '+ str(198//100))
+# print('batches '+str(recipe_batches({ 'milk': 100, 'butter': 50, 'cheese': 10 }, { 'milk': 198, 'butter': 52, 'cheese': 10 })))
+
+   
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 100, 'butter': 50, 'cheese': 10 }
+  ingredients = { 'milk': 198, 'butter': 52, 'cheese': 10 }
+  
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
