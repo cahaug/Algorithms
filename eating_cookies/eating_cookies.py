6e0f520d2621707b3ba4e5647b8dfb290c687719
cahaug <caleahaug@gmail.com>
2020-03-08 15:06:45 -0700

    eating_cookies and RPS passing

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,12 +1,65 @@
 #!/usr/bin/python
 
 import sys
+import math
+
+
+# 4 ways to eat 3 cookies 
+# 1+1+1
+# 1+2
+# 2+1
+# 3
+# 7 ways to eat 4 Cookies
+# 1+1+1+1
+# 1+1+2
+# 1+2+1
+# 1+3
+# 2+1+1
+# 2+2
+# 3+1
+# 13 ways to eat 5 cookies
+# 1+1+1+1+1
+# 1+1+1+2
+# 1+1+2+1
+# 1+2+1+1
+# 2+1+1+1
+# 1+1+3
+# 1+3+1
+# 3+1+1
+# 2+2+1
+# 2+1+2
+# 1+2+2
+# 2+3
+# 3+2
+# #
+
+
+
+
+
+
 
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+def eating_cookies(n, cache=dict()):
+  # check if the n has already been calculate
+  if n in cache:
+    return cache[n]
+  # base case, he cannot eat anymore cookies
+  if n <= 0:
+    return 1
+  way_counter = 0
+  for i in range(1, 4):
+    if i < n:
+      way_counter += eating_cookies(n - i)
+    elif i == n:
+      way_counter += 1
+  # save the result of n into cache for reuse
+  cache[n] = way_counter
+  return way_counter
+  
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,31 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  options = ["rock", "paper", "scissors"]
+
+  def rps_recursive(n):
+    nonlocal options
+    permutations = list()
+    if n == 0:
+      permutations.append([])
+    elif n <= 1:
+      for option in options:
+        permutations.append([option])
+    else:
+      for option in options:
+        for newPermutation in rps_recursive(n-1):
+          newPermutation.insert(0, option)
+          permutations.append(newPermutation)
+    return permutations
+  return rps_recursive(n)
+
+
+
+print(rock_paper_scissors(0))
+print(rock_paper_scissors(1))
+
+# print(rock_paper_scissors(2))
+options = [['rock'], ['paper'], ['scissors']]
 
 
 if __name__ == "__main__":
